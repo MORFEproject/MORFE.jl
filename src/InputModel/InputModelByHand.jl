d31b33dde98393fdd5042dc2ff35765026864831
Florian Tuschner <florian.tuschner@imfd.tu-freiberg.de>
2026-03-11 08:54:13 +0100

    rename InputModelAbstract to AbstractInputModel

--- src/InputModel/InputModelByHand.jl
+++ src/InputModel/InputModelByHand.jl
@@ -8,7 +8,7 @@ InputModelByHand implements an ODE system of second order:
     M d_t² U  +  C d_t U + KU  +  F_nl(U) = F(t) 
 with the nonlinearity up to order 3. C has to be calculated by modal damping, and F(t) as linearcombination of eigenmodes.
 =#
-mutable struct InputModelByHand <: InputModelAbstractSecondOrder
+mutable struct InputModelByHand <: AbstractInputModelSecondOrder
     mesh::Grid
     U::Field
     dim
@@ -19,25 +19,25 @@ mutable struct InputModelByHand <: InputModelAbstractSecondOrder
 end
 
 function InputModelByHand(info_file::String, mesh_file::String)
-  include(info_file)
-  dim = 3
-  mesh = read_mesh(mesh_file,domains_list,materials_list,materials_dict,
-                 boundaries_list,constrained_dof,bc_vals)
-  U = Field(mesh, dim)
+    include(info_file)
+    dim = 3
+    mesh = read_mesh(mesh_file, domains_list, materials_list, materials_dict,
+        boundaries_list, constrained_dof, bc_vals)
+    U = Field(mesh, dim)
 
-  return InputModelByHand(
-    mesh,
-    U,
-    dim,
-    nothing, 
-    nothing,
-    nothing
-  )
+    return InputModelByHand(
+        mesh,
+        U,
+        dim,
+        nothing,
+        nothing,
+        nothing
+    )
 end
 
 function assemble!(fem::InputModelByHand)
     colptr, rowval = Morfe_2_0.assembler_dummy_MK(fem.mesh, fem.U)
-    val = zeros(Float64,length(rowval))
+    val = zeros(Float64, length(rowval))
     fem.K = SparseMatrixCSC(fem.U.neq, fem.U.neq, colptr, rowval, val)
     fem.M = deepcopy(fem.K)
     Morfe_2_0.assembler_MK!(fem.mesh, fem.U, fem.K, fem.M)
@@ -55,14 +55,14 @@ function assemble_load_vector!(fem::InputModelByHand)
     error("Use eigenmode as load vector!")
 end
 
-function evaluate_quadratic_nonlinearity(fem::InputModelByHand, Ψ₁,Ψ₂)
-    res = zeros(2*fem.U.neq)
+function evaluate_quadratic_nonlinearity(fem::InputModelByHand, Ψ₁, Ψ₂)
+    res = zeros(2 * fem.U.neq)
     Morfe_2_0.assembly_G!(res, Ψ₁, Ψ₂, fem.U, fem.mesh)
     return res
 end
 
-function evaluate_cubic_nonlinearity(fem::InputModelByHand, Ψ₁,Ψ₂,Ψ₃)
-    res = zeros(2*fem.U.neq)
+function evaluate_cubic_nonlinearity(fem::InputModelByHand, Ψ₁, Ψ₂, Ψ₃)
+    res = zeros(2 * fem.U.neq)
     Morfe_2_0.assembly_H!(res, Ψ₁, Ψ₂, Ψ₃, fem.U, fem.mesh)
     return res
 end
@@ -75,14 +75,14 @@ function mass_matrix(fem::InputModelByHand)
 end
 
 function stiffness_matrix(fem::InputModelByHand)
-     if isnothing(fem.K)
+    if isnothing(fem.K)
         assemble_system!(fem)
     end
     return fem.K
 end
 
 function load_vector(fem::InputModelByHand)
-     if isnothing(fem.F)
+    if isnothing(fem.F)
         assemble_system!(fem)
     end
     return fem.F
--- src/InputModel/InputModelInterface.jl
+++ src/InputModel/InputModelInterface.jl
@@ -1,34 +1,39 @@
 #=
-InputModelAbstract expects first order ODE-System:
+AbstractInputModel expects first order ODE-System:
     B d_t X = AX + F_nl(X) + F(t)
 =#
-abstract type InputModelAbstract end
+abstract type AbstractInputModel end
 
-function a_matrix(fem::InputModelAbstract)
+function assemble!(fem::AbstractInputModel)
+    error("assemble! not implemented for $(typeof(fem))")
+end
+
+function a_matrix(fem::AbstractInputModel)
     error("a_matrix not implemented for $(typeof(fem))")
 end
 
-function b_matrix(fem::InputModelAbstract)
+function b_matrix(fem::AbstractInputModel)
     error("b_matrix not implemented for $(typeof(fem))")
 end
 
-function f_vector(fem::InputModelAbstract)
+function f_vector(fem::AbstractInputModel)
     error("f_vector not implemented for $(typeof(fem))")
 end
 
-function evaluate_nonlinearity(fem::InputModelAbstract, Ψ...)
+
+function evaluate_nonlinearity(fem::AbstractInputModel, Ψ...)
     error("evaluate_nonlinearity not implemented for $(typeof(fem))")
 end
 
-function ndofs(fem::InputModelAbstract)
+function ndofs(fem::AbstractInputModel)
     error("ndofs not implemented for $(typeof(fem))")
 end
 
-function field_from_vector(fem::InputModelAbstract, u)
+function field_from_vector(fem::AbstractInputModel, u)
     error("field_from_vector not implemented for $(typeof(fem))")
 end
 
-function visualize(fem::InputModelAbstract, u; kwargs...)
+function visualize(fem::AbstractInputModel, u; kwargs...)
     error("visualize not implemented for $(typeof(fem))")
 end
 
@@ -36,34 +41,55 @@ end
 Representation of second order ODE-System:
     M d_t² U  +  C d_t U + KU  +  F_nl(U) = F(t)
 =#
-abstract type InputModelAbstractSecondOrder <: InputModelAbstract end
+abstract type AbstractInputModelSecondOrder <: AbstractInputModel end
 
-function assemble!(fem::InputModelAbstractSecondOrder)
+function assemble!(fem::AbstractInputModelSecondOrder)
     assemble_mass_matrix!(fem)
     assemble_stiffness_matrix!(fem)
+    assemble_damping_matrix!(fem)
     assemble_load_vector!(fem)
 end
 
-function assemble_mass_matrix!(fem::InputModelAbstractSecondOrder)
+function assemble_mass_matrix!(fem::AbstractInputModelSecondOrder)
     error("assemble_mass_matrix! not implemented for $(typeof(fem))")
 end
 
-function assemble_stiffness_matrix!(fem::InputModelAbstractSecondOrder)
+function assemble_stiffness_matrix!(fem::AbstractInputModelSecondOrder)
     error("assemble_stiffness_matrix! not implemented for $(typeof(fem))")
 end
 
-function assemble_load_vector!(fem::InputModelAbstractSecondOrder)
+function assemble_damping_matrix!(fem::AbstractInputModelSecondOrder)
+    error("assemble_damping_matrix! not implemented for $(typeof(fem))")
+end
+
+function assemble_load_vector!(fem::AbstractInputModelSecondOrder)
     error("assemble_load_vector! not implemented for $(typeof(fem))")
 end
 
-function a_matrix(fem::InputModelAbstractSecondOrder)
+function a_matrix(fem::AbstractInputModelSecondOrder)
     error("For $(typeof(fem)) use structure of second order system!")
 end
 
-function b_matrix(fem::InputModelAbstractSecondOrder)
+function b_matrix(fem::AbstractInputModelSecondOrder)
     error("For $(typeof(fem)) use structure of second order system!")
 end
 
-function f_vector(fem::InputModelAbstractSecondOrder)
+function f_vector(fem::AbstractInputModelSecondOrder)
     error("For $(typeof(fem)) use structure of second order system!")
+end
+
+function mass_matrix(fem::AbstractInputModelSecondOrder)
+    error("mass_matrix not implemented for $(typeof(fem))")
+end
+
+function stiffness_matrix(fem::AbstractInputModelSecondOrder)
+    error("stiffness_matrix not implemented for $(typeof(fem))")
+end
+
+function damping_matrix(fem::AbstractInputModelSecondOrder)
+    error("damping_matrix not implemented for $(typeof(fem))")
+end
+
+function load_vector(fem::AbstractInputModelSecondOrder)
+    error("load_vector not implemented for $(typeof(fem))")
 end
